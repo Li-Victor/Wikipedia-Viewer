6033071299b3cb4800a642ed48003b3ceee9ffd6
victorli5674 <victorli5674@yahoo.com>
2017-01-20 23:29:39 -0800

    finished making requests with the wikipedia api

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$document.ready(function() {
-    
+$(document).ready(function() {
+    var jqxhr = $.getJSON('https://en.wikipedia.org/w/api.php?' +
+        'action=opensearch&format=json&limit=10&profile=classic&callback=?' +
+        '&search=' + $("#searchBar").val())
+        .done(function(response) {
+            console.log(response);
+    });
+
 })
