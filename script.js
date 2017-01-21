99f3eb241d588822b8ca9bf47c3d3ae90762cd82
victorli5674 <victorli5674@yahoo.com>
2017-01-21 05:06:02 -0800

    searching for entries now gives a list of searches

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-    var jqxhr = $.getJSON('https://en.wikipedia.org/w/api.php?' +
-        'action=opensearch&format=json&limit=10&profile=classic&callback=?' +
-        '&search=' + $("#searchBar").val())
-        .done(function(response) {
-            console.log(response);
+
+    $('#submit').on('click', function() {
+        $('.list').html('');
+
+        var jqxhr = $.getJSON('https://en.wikipedia.org/w/api.php?' +
+            'action=opensearch&format=json&limit=10&profile=classic&callback=?' +
+            '&search=' + $('#searchBar').val())
+            .done(function(response) {
+
+                for (var i = 0; i < response[1].length; i++) {
+                    $('.list').append('<dl> <dt> <a href="' + response[3][i] + '" target="_blank">' + response[1][i] + '</a></dt> <dd>' + response[2][i] + '</dd>  </dl>')
+                }
+        });
     });
 
 })
